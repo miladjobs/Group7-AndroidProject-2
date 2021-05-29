0f20b2a884c05503ef4d05e0cce7c0c24449bbc4
Milad Esfandiyari <miladisjobs@gmail.com>
2021-05-29 21:58:27 +0200

    complete main activity

--- app/src/main/java/ir/iust/group7_androidproject_2/MainActivity.kt
+++ app/src/main/java/ir/iust/group7_androidproject_2/MainActivity.kt
@@ -1,7 +1,9 @@
 package ir.iust.group7_androidproject_2
 
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
+import androidx.activity.viewModels
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
@@ -13,6 +15,10 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import ir.iust.group7_androidproject_2.ui.file_detail.FileViewModel
+import ir.iust.group7_androidproject_2.utils.DataInjector
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,7 +27,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val factory = DataInjector.provideFileViewModelFactory()
+        val viewModel = ViewModelProvider(this, factory).get(FileViewModel::class.java)
+        viewModel.initializeData()
+        Log.i("MainActivity", "initialize app and fetch data")
+        viewModel.getFiles().observe(this, Observer { files -> files.forEach{
+            file -> Log.i("MainActivity", "fetch $file")
+        } })
 
     }
 
