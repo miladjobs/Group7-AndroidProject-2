afe37a3295040683639c302fa981450e5d8680e8
Milad Esfandiyari <miladisjobs@gmail.com>
2021-05-30 09:11:27 +0200

    add back button in file detail page

--- app/src/main/java/ir/iust/group7_androidproject_2/ui/file_detail/FileDetailFragment.kt
+++ app/src/main/java/ir/iust/group7_androidproject_2/ui/file_detail/FileDetailFragment.kt
@@ -6,17 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.lifecycle.ViewModelProvider
 import ir.iust.group7_androidproject_2.R
 import ir.iust.group7_androidproject_2.data.File
+import ir.iust.group7_androidproject_2.ui.home.HomeFragment
 import ir.iust.group7_androidproject_2.utils.DataInjector
 
 
 class FileDetailFragment: Fragment() {
     private var file_id: Int = -1
     private var param2: String? = null
+    private lateinit var btnBack: Button
     private lateinit var fileImage: ImageView
     private lateinit var fileName: TextView
     private lateinit var fileDownloadsCount: TextView
@@ -37,6 +40,7 @@ class FileDetailFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.fragment_file_detail, container, false)
+        btnBack = view.findViewById(R.id.btn_detail_back)
         fileImage = view.findViewById(R.id.file_image)
         fileDescription = view.findViewById(R.id.file_description)
         fileName = view.findViewById(R.id.file_name)
@@ -51,6 +55,18 @@ class FileDetailFragment: Fragment() {
         fileSize.text = """${file.size} KB"""
         fileDownloadsCount.text = file.downloadsCount.toString()
         fileImage.setImageResource(file.image_path)
+
+        btnBack.setOnClickListener{
+            Log.i("HomeFragment", "back from detail clicked")
+            val homeFragment = HomeFragment()
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+            if(transaction != null){
+                transaction.replace(R.id.nav_host_fragment, homeFragment)
+                transaction.addToBackStack(null)
+                transaction.commit()
+            }
+        }
+
         return view
     }
 
